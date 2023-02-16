4abe961b784b8161448b032f1a481975eb7933eb
Elias Spector-Zabusky <elias.spector.zabusky@gmail.com>
2023-02-16 11:40:20 -0500

    initial commit with pseudo-code

---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+// DOM Dependencies
+  // Header
+    // Real Time (dayjs)
+
+  // Card
+    // Bootstrap Card w/ instructions
+
+  // Table
+    // Bootstrap Table
+    // Local Storage
+
+  // Modal
+    // Bootstrap Modal
+      // Form
+    
+// Data
+  // Header
+    // dayjs
+
+  // Card
+    // Instructions
+
+  // Table
+    // Local Storage saved user info
+
+  // Modal
+    // No direct data
+
+// Functions
+  // Header
+    // JS for set interval
+
+  // Card
+    // Button to activate Modal
+
+  // Table
+    // Grabbing local storage initially
+  
+  // Modal
+    // Save data to local storage and print in table
+
+// User Interactions
+  // Header
+    // No interaction
+
+  // Card
+    // Button to activate Modal
+
+  // Table
+    // Delete button
+
+  // Modal
+    // Form and submit
+
+// Initializations
+  // Header
+    // On page load
+
+  // Card
+    // On page load
+
+  // Table
+    // On page load
+
+  // Modal
+    // Button on Card
